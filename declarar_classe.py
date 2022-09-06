20abd6e6990f14e6eb9d19db5e6359f39f71d558
Carlos Kelvis <kekehacct@gmail.com>
2022-09-06 09:35:08 -0300

    Create declarar_classe.py

---
+++ declarar_classe.py
@@ -0,0 +1,15 @@
+# A Classe é uma abstração de entidades (objetos) do mundo real.
+
+
+class Pessoa:
+    pass
+
+
+# OBJETOS
+p1 = Pessoa()
+p2 = Pessoa()
+
+# Note que p1 e p2 recebem a mesma classe. Porém, são dois objetos diferentes.
+
+print(p1)
+print(p2)
